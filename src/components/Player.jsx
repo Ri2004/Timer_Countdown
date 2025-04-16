79114b66b5e6017bec8c449e43a93b0f2b2140bb
Rishabh Chatterjee <rishabhchatterjee813@gmail.com>
2025-04-16 20:23:00 +0530

    Old code added

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,25 @@
 import { useState, useRef } from "react";
 
+// Using two way binding
+// export default function Player() {
+//   const [enteredPlayerName, setEnteredPlayerName] = useState("");
+//   const [submitted, setSubmitted] = useState(false);
+
+//   const handleClick = function () {
+//     setSubmitted(true);
+//   };
+
+//   return (
+//     <section id="player">
+//       <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
+//       <p>
+//         <input type="text" value={enteredPlayerName} onChange={(e) => setEnteredPlayerName(e.target.value)} />
+//         <button onClick={handleClick}>Set Name</button>
+//       </p>
+//     </section>
+//   );
+// }
+
 export default function Player() {
   const playerName = useRef();
   const [enteredPlayerName, setEnteredPlayerName] = useState(null);
